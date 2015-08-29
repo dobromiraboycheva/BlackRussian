496c7a2f828105f9f178b12407529bbebdd3b487
Todor Mitskovski <todorbm85@gmail.com>
2015-08-30 01:45:39 +0300

    Add draw board

--- app/gameEngine/gameManager.js
+++ app/gameEngine/gameManager.js
@@ -5,15 +5,15 @@ import player from 'app/gameObjects/player.js';
 var gameManager = function() {
 
     // this method should be used to initialize the gameManager`s instance when created
-    function init(uiProvider, logicProvider, playersCount) {
-        this.uiProvider = uiProvider;
+    function init(logicProvider, playersCount) {
+
         this.logicProvider = logicProvider;
 
         this.tilesPool = logicProvider.getGameTiles();
         this.board = [];
 
         this.players = getPlayers(playersCount);
-        this.players.forEach(function (player) {
+        this.players.forEach(function(player) {
             logicProvider.giveNewTilesToPlayer(player, this.tilesPool);
         }, this);
         this.currentPlayer = this.players[0];
@@ -23,17 +23,20 @@ var gameManager = function() {
     }
 
     // this is the main game loop
-    function startGame() {
-        console.log('game started!');
-        // while (!gameOver) {
-        //     var newBoard = uiProvider.makeMove(currentPlayer, board);            
-        //     if (!logicProvider.validateBoard(newBoard)) {
-        //         uiProvider.message("Invalid move!");
-        //         continue;
-        //     }
+    function makeMove(newBoard) {
+        var response = {
+            board: this.board,
+            player: currentPlayer,
+            msg: '',
+        };
 
-        //     updateGameState(newBoard);
-        // }
+        if (logicProvider.isBoardValid(newBoard)) {
+            updateGameState(newBoard);
+        } else {
+            response.msg = 'Invalid move!';
+        }
+
+        return response;
     }
 
     function updateGameState(newBoard) {
@@ -48,16 +51,16 @@ var gameManager = function() {
     // when a new game instance is created and initialized
     function getPlayers(playersCount) {
         var players = [];
-        for (var i = 0; i < playersCount; i+=1) {
+        for (var i = 0; i < playersCount; i += 1) {
             players.push(Object.create(player).init('Player ' + i));
         }
-        
+
         return players;
     }
 
     return {
         init: init,
-        startGame: startGame,
+        makeMove: makeMove,
     };
 }();
 
