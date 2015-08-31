b7f019663ba8e92ffa56a4af8524b7f679b97f7a
zmihaylov <zlatimirmihailov@gmail.com>
2015-08-31 15:26:21 +0300

    Change the way to generate the tiles
    The tiles are generated in order and the function generating them is returned!

--- app/gameObjects/tile.js
+++ app/gameObjects/tile.js
@@ -1,41 +1,33 @@
 var tile = function() {
-    var LETTER_POINTS = {
-        'a': 1,
-        'b': 1,
-        'c': 1,
-        'd': 1,
-        'e': 1,
-        'f': 1,
-        'g': 1,
-        'h': 1,
-        'i': 1,
-        'j': 1,
-        'k': 1,
-        'l': 1,
-        'm': 1,
-        'n': 1,
-        'o': 1,
-        'p': 1,
-        'q': 1,
-        'r': 1,
-        's': 1,
-        't': 1,
-        'u': 1,
-        'v': 1,
-        'w': 1,
-        'x': 1,
-        'y': 1,
-        'z': 1,
+    var allTiles = [],
+        LETTER_POINTS = {
+        '0': {'blank':2},
+        '1': {'a':9, 'e':8, 'i':8, 'n':4, 'o':9,'p':4, 'r':4, 's':4,'t':5},
+        '2': {'b':3,'v':4,'d':4,'k':3,'l':3,'m':4},
+        '3': {'g':3, 'aa':2},
+        '4': {'j':2,'z':2},
+        '5': {'ikaratko':1,'u':3,'h':1,'ch':2,'q':2},
+        '8': {'c':1,'sh':1,'iu':1},
+        '10': {'f':1,'sht':1,'ermaluk':1}
     };
 
-    function init(letter) {
-        this.letter = letter;
-        this.points = LETTER_POINTS[letter.toLowerCase()];
-        return this;
+    function getAllTiles() {
+        for (var points in LETTER_POINTS) {
+
+            for (var letter in LETTER_POINTS[points]) {
+                //console.log(letter + ' ' + LETTER_POINTS[points][letter]);
+
+                for (var count = 0; count < LETTER_POINTS[points][letter]; count++) {
+                    allTiles.push({ letter: letter, points: points });
+                }
+            }
+        }
+
+        return allTiles;
     }
 
     return {
-        init: init
+        getAllTiles: getAllTiles
     }
 }();
 
